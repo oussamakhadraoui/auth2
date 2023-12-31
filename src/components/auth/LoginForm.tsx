23ff3693b2719b45e8e51879a141f38ca55666ac
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-12-31 23:50:09 +0100

    login form implementation

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,9 +1,27 @@
+'use client'
 import React from 'react'
 import CardWrap from './CardWrap'
-
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { LoginSchema, LoginType } from '@/lib/validation'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import FormError from '../FormError'
+import FormSuccess from '../FormSuccess'
 interface LoginFormProps {}
 
 const LoginForm = ({}: LoginFormProps) => {
+  const form = useForm<LoginType>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+  const onSubmit = (values: LoginFormProps) => {
+console.log(values)
+  }
   return (
     <CardWrap
       backButtonLabel="Don't have an account"
@@ -11,7 +29,47 @@ const LoginForm = ({}: LoginFormProps) => {
       headerLabel='Welcome Back'
       showSocial
     >
-      LoginForm
+      <Form {...form}>
+        <form className='space-y-6' onSubmit={form.handleSubmit(onSubmit)}>
+          <div className='space-y-4'>
+            <FormField
+              name='email'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder='email@exmaple.com'
+                      type='email'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name='password'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder='******' type='password' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message=""/>
+          <FormSuccess message=''/>
+          <Button type='submit' className='w-full'>
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrap>
   )
 }
