8515621988f0eb294c3aff1f4e6e9bb8a231abd0
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-01 23:18:44 +0100

    github and google provider login

--- src/components/auth/Social.tsx
+++ src/components/auth/Social.tsx
@@ -2,12 +2,16 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
-
+import {signIn} from 'next-auth/react'
 import { Button } from '../ui/button'
+
+import { DEFAULT_LOGIN_REDIRECT } from '../../../routes'
 interface SocialProps {}
 
 const Social = ({}: SocialProps) => {
- const onClick = (social: 'github' | 'google') => {}
+ const onClick = (social: 'github' | 'google') => {
+ signIn(social, { callbackUrl: DEFAULT_LOGIN_REDIRECT })
+ }
   return (
     <div className='flex items-center w-full gap-x-2'>
       <Button
