002c82155f49b5bd5868e0f2b8594fa61627e16e
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-01 23:17:46 +0100

    schema and types

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -4,4 +4,16 @@ export const LoginSchema = z.object({
  email:z.string().email({message:"Invalid Email"}),
  password:z.string({required_error:'This field is required'}).min(4,{message:"Need a longer password."})
 })
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z.string().min(6, {
+    message: 'Minimum 6 characters required',
+  }),
+  name: z.string().min(1, {
+    message: 'Name is required',
+  }),
+})
+export type registerType= z.infer<typeof RegisterSchema>
 export type LoginType= z.infer<typeof LoginSchema>
