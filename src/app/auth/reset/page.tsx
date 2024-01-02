693c914c5067fcb468455ec33288c59f013a8ca7
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-02 23:38:43 +0100

    reset password main page

---
+++ src/app/auth/reset/page.tsx
@@ -0,0 +1,12 @@
+import ResetForm from '@/components/auth/ResetForm'
+import React from 'react'
+
+interface pageProps {
+  
+}
+
+const page = ({}: pageProps) => {
+  return < ResetForm/>
+}
+
+export default page
