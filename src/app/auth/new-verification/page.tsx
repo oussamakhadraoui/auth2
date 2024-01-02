2d6ecd1004e84e459e537c0abb52a52ecb4e2a25
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-02 23:40:41 +0100

    verification main page

---
+++ src/app/auth/new-verification/page.tsx
@@ -0,0 +1,14 @@
+
+import NewVerificationForm from '@/components/auth/NewverificationForm'
+import React from 'react'
+
+interface pageProps {
+  
+}
+
+const Page = ({}: pageProps) => {
+
+  return <NewVerificationForm/>
+}
+
+export default Page
