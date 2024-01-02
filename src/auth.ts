d61c2065a63ba212404eb0306c254a3be8fb86ff
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-02 23:31:37 +0100

    auth implementation

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
-import NextAuth, { type DefaultSession } from 'next-auth'
+import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaClient, UserRole } from '@prisma/client'
 import authConfig from './auth.config'
 import { getUserById } from './components/Data/user'
+import { db } from './lib/db'
 
 const prisma = new PrismaClient()
 export const {
@@ -14,7 +15,28 @@ export const {
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,
+  pages: {
+    signIn: '/auth/login',
+    error: '/auth/error',
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date(),
+        },
+      })
+    },
+  },
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== 'credentials') return true
+      const existenceUser = await getUserById(user.id)
+      //prevent sign inn without email verification
+      if (!existenceUser?.emailVerified) return false
+      return true
+    },
     async jwt({ token }) {
       if (!token.sub) return token
       const user = await getUserById(token.sub)
@@ -22,6 +44,7 @@ export const {
       token.role = user.role
       return token
     },
+
     async session({ token, session }) {
       if (session.user && token.sub) {
         session.user.id = token.sub
