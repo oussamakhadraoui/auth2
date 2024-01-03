682c9f9b9e367847d74fa00147c991b17e723dd9
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-03 23:46:04 +0100

    reset password server action

--- actions/reset.ts
+++ actions/reset.ts
@@ -1,6 +1,24 @@
-export const reset=async (email:string)=>{
+"use server"
 
+import { getUserByEmail } from '@/components/Data/user';
+import { generatePassToken } from '@/lib/GenerateToken';
+import { sendResetPasswordToken } from '@/lib/emailSender';
+import { ResetSchema, ResetType } from '@/lib/validation';
 
+export const reset=async (values:ResetType)=>{
+const validation = ResetSchema.safeParse(values)
+if(!validation.success){
+ return {error:"missing email"}
+}
+const {email}= values
+
+const user = await getUserByEmail(email)
+if(!user){
+ return { error: 'Email Not found' }
+}
+const passResetToken =await generatePassToken(email)
+await sendResetPasswordToken(passResetToken.email, passResetToken.token)
+return{success:'reset email send'}
 
 
 }
