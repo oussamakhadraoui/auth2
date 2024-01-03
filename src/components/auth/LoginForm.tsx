434d43c46c64e80ad7e07bd88cb4153a6a4deaed
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-03 23:57:05 +0100

    login form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -28,6 +28,7 @@ const LoginForm = ({}: LoginFormProps) => {
     search.get('error') === 'OAuthAccountNotLinked'
       ? 'This email is used by a different user try login with another account!'
       : ''
+  const [show2fact, setShow2Fact] = useState<boolean>(false)
   const [error, setError] = useState<string | undefined>('')
   const [success, setSuccess] = useState<string | undefined>('')
   const form = useForm<LoginType>({
@@ -35,16 +36,28 @@ const LoginForm = ({}: LoginFormProps) => {
     defaultValues: {
       email: '',
       password: '',
+      
     },
   })
   const onSubmit = (values: LoginType) => {
     setError('')
     setSuccess('')
     startTransition(() => {
-      login(values).then((data) => {
-        setError(data?.error)
-        setSuccess(data?.success)
-      })
+      login(values)
+        .then((data) => {
+          if (data.error) {
+            form.reset()
+            setError(data.error)
+          }
+          if (data.success) {
+            form.reset()
+            setSuccess(data.success)
+          }
+          if (data.twoFactor) {
+            setShow2Fact(true)
+          }
+        })
+        .catch(() => setError('something went wrong'))
     })
   }
   return (
@@ -57,55 +70,79 @@ const LoginForm = ({}: LoginFormProps) => {
       <Form {...form}>
         <form className='space-y-6' onSubmit={form.handleSubmit(onSubmit)}>
           <div className='space-y-4'>
-            <FormField
-              name='email'
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder='email@exmaple.com'
-                      type='email'
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name='password'
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder='******'
-                      type='password'
-                    />
-                  </FormControl>
-                  <Button
-                    size={'sm'}
-                    variant={'link'}
-                    asChild
-                    className='px-0 font-normal'
-                  >
-                    <Link href='/auth/reset'>Forget password</Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {show2fact && (
+              <FormField
+                name='code'
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Code</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder='123456'
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            {!show2fact && (
+              <>
+                {' '}
+                <FormField
+                  name='email'
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder='email@exmaple.com'
+                          type='email'
+                          disabled={isPending}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name='password'
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          disabled={isPending}
+                          placeholder='******'
+                          type='password'
+                        />
+                      </FormControl>
+                      <Button
+                        size={'sm'}
+                        variant={'link'}
+                        asChild
+                        className='px-0 font-normal'
+                      >
+                        <Link href='/auth/reset'>Forget password</Link>
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
           </div>
           <FormError message={error || isAlreadyExist} />
           <FormSuccess message={success} />
           <Button type='submit' disabled={isPending} className='w-full'>
-            Login
+           {show2fact?"Confirm":'Login'}
           </Button>
         </form>
       </Form>
