450c86832fc46d0954c1daf36488bae58882598c
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-03 23:56:16 +0100

    new password main page

---
+++ src/app/auth/new-password/page.tsx
@@ -0,0 +1,12 @@
+import NewPassForm from '@/components/auth/NewPassForm'
+import React from 'react'
+
+interface pageProps {
+  
+}
+
+const page = ({}: pageProps) => {
+  return <NewPassForm/>
+}
+
+export default page
