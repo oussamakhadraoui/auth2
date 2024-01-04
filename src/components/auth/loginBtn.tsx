8cd30d3bd4a5eb9f68eeacccd5b44d75df9ad501
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-04 21:16:29 +0100

    login form modal

--- src/components/auth/loginBtn.tsx
+++ src/components/auth/loginBtn.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import LoginForm from './LoginForm'
+import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
 
 interface loginBtnProps {
   children: React.ReactNode
@@ -10,12 +12,22 @@ interface loginBtnProps {
 
 const LoginBtn = ({ children, asChild, mode = 'redirect' }: loginBtnProps) => {
   const router = useRouter()
+
   const onClick = () => {
     router.push('/auth/login')
   }
+
   if (mode === 'modal') {
-    return <span>TODO later</span>
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className='p-0 w-auto bg-transparent border-none'>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    )
   }
+
   return (
     <span onClick={onClick} className='cursor-pointer'>
       {children}
