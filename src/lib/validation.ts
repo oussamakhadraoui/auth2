0d5a5a4128c4237389c89ba78c2465ff1b915bc8
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-04 21:12:57 +0100

    schema and types

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,3 +1,4 @@
+import { UserRole } from '@prisma/client'
 import z from 'zod'
 
 export const LoginSchema = z.object({
@@ -28,8 +29,43 @@ export const RegisterSchema = z.object({
   }),
 })
 
-export type registerType = z.infer<typeof RegisterSchema>
+export const SettingSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false
+      }
 
+      return true
+    },
+    {
+      message: 'New password is required!',
+      path: ['newPassword'],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) {
+        return false
+      }
+
+      return true
+    },
+    {
+      message: 'Password is required!',
+      path: ['password'],
+    }
+  )
+export type typeSettings = z.infer<typeof SettingSchema>
+export type registerType = z.infer<typeof RegisterSchema>
 export type newPassType = z.infer<typeof newPassSchema>
 export type LoginType = z.infer<typeof LoginSchema>
 export type ResetType = z.infer<typeof ResetSchema>
